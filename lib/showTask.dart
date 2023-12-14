10494b0547a0d41bb6a34839d08059fe50b46cf4
Abdul Salam <151502342+SaeedanTechnologies@users.noreply.github.com>
2023-12-14 16:31:13 +0500

    commit

--- lib/showTask.dart
+++ lib/showTask.dart
@@ -17,75 +17,64 @@ class _ShowTaskState extends State<ShowTask> {
   TextEditingController taskController = TextEditingController();
   String username = PreferencesManager.instance.getUserName();
 
-
-Future<void> _updateTask(String id,DateTime deadlines) async {
- 
-
+  Future<void> _updateTask(String id, DateTime deadlines) async {
     final currentTime = DateTime.now();
     final deadline = deadlines;
- final Duration difference = currentTime.difference(deadline);
+    final Duration difference = currentTime.difference(deadline);
 
     if (difference.isNegative) {
       // Time exceeds the deadline
       final exceededTime = -difference;
-      final formattedExceededTime =
-          DateFormat.Hms().format(DateTime(0, 1, 1, exceededTime.inHours, exceededTime.inMinutes % 60));
+      final formattedExceededTime = DateFormat.Hms().format(
+          DateTime(0, 1, 1, exceededTime.inHours, exceededTime.inMinutes % 60));
       print('Time Exceeded by $formattedExceededTime');
-         await FirebaseFirestore.instance
-        .collection('tasks')
-        .doc(id)
-        .update({
-      'status': true,
-      'completeTime': currentTime,
-      "onTime":"Completed on Time",
-    });
+      await FirebaseFirestore.instance.collection('tasks').doc(id).update({
+        'status': true,
+        'completeTime': currentTime,
+        "onTime": "Completed on Time",
+      });
       // return formattedExceededTime.toString();
     } else {
-          await FirebaseFirestore.instance
-        .collection('tasks')
-        .doc(id)
-        .update({
-      'status': true,
-      'completeTime': currentTime,
-      "onTime":"Completed on Time",
-    });
+      await FirebaseFirestore.instance.collection('tasks').doc(id).update({
+        'status': true,
+        'completeTime': currentTime,
+        "onTime": "Completed on Time",
+      });
     }
     // Update task status and complete time
- 
 
     // Reload tasks
     // _loadTasks();
 
     // Calculate and show the time difference
-   
   }
 
-    Future<String?> _calculateTimeDifference(DateTime currentTime, DateTime deadline)async {
-   
-    
-  }
+  Future<String?> _calculateTimeDifference(
+      DateTime currentTime, DateTime deadline) async {}
 
   @override
   Widget build(BuildContext context) {
     print(username);
     return Scaffold(
-      
         appBar: AppBar(
           automaticallyImplyLeading: false,
           title: Text('Task Assignment App'),
-          actions: [  ElevatedButton(
-                  onPressed: ()async{
-                      await FirebaseAuth.instance.signOut();
-                      Get.to(()=>LoginScreen());
-                    PreferencesManager.instance.removeUserName();
-                  },
-                  child: Text('Sign out'),
-                ),],
+          actions: [
+            ElevatedButton(
+              onPressed: () async {
+                await FirebaseAuth.instance.signOut();
+                Get.to(() => LoginScreen());
+                PreferencesManager.instance.removeUserName();
+              },
+              child: Text('Sign out'),
+            ),
+          ],
         ),
         body: StreamBuilder<QuerySnapshot>(
           stream: FirebaseFirestore.instance
               .collection('tasks')
-              .where("idUser", isEqualTo: username).where("status",isEqualTo: false)
+              .where("idUser", isEqualTo: username)
+              .where("status", isEqualTo: false)
               .snapshots(),
           builder: (context, snapshot) {
             if (snapshot.connectionState == ConnectionState.waiting) {
@@ -101,67 +90,70 @@ Future<void> _updateTask(String id,DateTime deadlines) async {
               //      List<String> docus = snapshot.data!.docs
               //     .map((doc) => doc.id)
               //     .toList();
-final documents = snapshot.data!.docs;
+              final documents = snapshot.data!.docs;
               return Container(
-                height: MediaQuery.of(context).size.height -40,
+                height: MediaQuery.of(context).size.height - 40,
                 width: MediaQuery.of(context).size.width,
                 child: ListView.builder(
                   scrollDirection: Axis.vertical,
                   itemCount: documents.length,
                   itemBuilder: (BuildContext context, int index) {
-                         var ids = documents[index].id;
-                       var cusName = documents[index]['startTime'];
-                var task = documents[index]['task'];
-                var diets = documents[index]['deadLine']; 
-                 var onTime = documents[index]['onTime']; 
-                 var totalTime = documents[index]['totalTime']; 
-                 var runn = documents[index]['isRunning']; 
-                 
+                    var ids = documents[index].id;
+                    var cusName = documents[index]['startTime'];
+                    var task = documents[index]['task'];
+                    var diets = documents[index]['deadLine'];
+                    var onTime = documents[index]['onTime'];
+                    var totalTime = documents[index]['totalTime'];
+                    var runn = documents[index]['isRunning'];
 
-                    return 
-                    Padding(
+                    return Padding(
                       padding: const EdgeInsets.all(8.0),
                       child: Container(
                         child: Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                          Text("Task:  " + task,style: TextStyle(fontSize: 25),),
-                          Text("Deadline  " + diets + " hour"),
-                          SizedBox(height: 10,),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                               ElevatedButton(
-                                    onPressed: () => runn == true ? pauseTimer(ids,cusName,totalTime) : startTimer(ids),
-                                    child: Text( runn == true ? 'Pause' : 'Start'),
-                                  ),
-                      
-                                  ElevatedButton(
-                                    onPressed: () =>
-                      completeTask(ids,cusName,totalTime,diets)
-                                    ,
-                                    child: Text('Complete'),
-                                  ),
-                              // Container(
-                              //   height: 50,
-                              //   width: 80,
-                              //   color: Colors.amber,
-                                
-                              //                     child: Center(child: Text("Complete")),
-                              // ),
-                            ],
-                          ),
-                        ],),
+                            Text(
+                              "Task:  " + task,
+                              style: TextStyle(fontSize: 25),
+                            ),
+                            Text("Deadline  " + diets + " hour"),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                ElevatedButton(
+                                  onPressed: () => runn == true
+                                      ? pauseTimer(ids, cusName, totalTime)
+                                      : startTimer(ids),
+                                  child: Text(runn == true ? 'Pause' : 'Start'),
+                                ),
+
+                                ElevatedButton(
+                                  onPressed: () => completeTask(
+                                      ids, cusName, totalTime, diets),
+                                  child: Text('Complete'),
+                                ),
+                                // Container(
+                                //   height: 50,
+                                //   width: 80,
+                                //   color: Colors.amber,
+
+                                //                     child: Center(child: Text("Complete")),
+                                // ),
+                              ],
+                            ),
+                          ],
+                        ),
                       ),
                     );
-                    
-                    
-                    
+
 //                     ListTile(
 //                       title: Text(task),
 //                       subtitle: Text(diets),
 //                       // subtitle: onTime =="onTime"? Text("Done on Time"):Text("Not in Time $cusName"),
-//                       trailing: 
+//                       trailing:
 //                       GestureDetector(
 //                         onTap: () async{
 // //  setState(() {
@@ -170,10 +162,9 @@ final documents = snapshot.data!.docs;
 //               // .collection('tasks').doc(ids).update({"status":true,"timestamp":DateTime.now()});
 //               // _updateTask(ids,diets);
 
-
 //  final currentTime = DateTime.now();
 //     final deadline = diets.toDate();
-    
+
 //  final Duration difference = currentTime.difference(deadline);
 
 //     if (difference.isNegative) {
@@ -205,7 +196,7 @@ final documents = snapshot.data!.docs;
 //     }
 //                             // });
 //                         },
-//                         child: 
+//                         child:
 //                         Row(
 //                           children: [
 //                              ElevatedButton(
@@ -222,13 +213,13 @@ final documents = snapshot.data!.docs;
 //                               height: 50,
 //                               width: 80,
 //                               color: Colors.amber,
-                              
+
 //                                                 child: Center(child: Text("Complete")),
 //                             ),
 //                           ],
 //                         ),
 //                       ),
-                    
+
 //                       // onTap: () {},
 //                     );
                   },
@@ -237,63 +228,51 @@ final documents = snapshot.data!.docs;
             }
           },
         ));
-  
   }
-
 
   bool isRunning = false;
 
+  void startTimer(String ids) async {
+    // setState(() {
+    //    isRunning = true;
+    // });
 
- void startTimer(String ids)async {
-  // setState(() {
-  //    isRunning = true; 
-  // });
-   
-       await FirebaseFirestore.instance
+    await FirebaseFirestore.instance
         .collection('tasks')
         .doc(ids)
-        .update({
-      'startTime': DateTime.now(),
-      "isRunning":true
-      
-    });
-     
+        .update({'startTime': DateTime.now(), "isRunning": true});
+
     //  currentTask.startTime = DateTime.now();
-    
   }
 
-  void pauseTimer(String ids,var ss,int totalTime)async {
+  void pauseTimer(String ids, var ss, int totalTime) async {
     // setState(() {
-    //   isRunning = false; 
+    //   isRunning = false;
     // });
-     
-       await FirebaseFirestore.instance
-        .collection('tasks')
-        .doc(ids)
-        .update({
-      'totalTime': totalTime + DateTime.now().difference(ss.toDate()).inMilliseconds,
-      "isRunning":false,
-      
+
+    await FirebaseFirestore.instance.collection('tasks').doc(ids).update({
+      'totalTime':
+          totalTime + DateTime.now().difference(ss.toDate()).inMilliseconds,
+      "isRunning": false,
     });
     //  currentTask.totalTime += DateTime.now().difference(currentTask.startTime).inMilliseconds;
-   
   }
 
-  void completeTask(String ids,var ss,int totalTime,String deadlinez) async {
+  void completeTask(String ids, var ss, int totalTime, String deadlinez) async {
     // setState(() {
-    // isRunning = false;     
+    // isRunning = false;
     // });
- 
-       await FirebaseFirestore.instance
-        .collection('tasks')
-        .doc(ids)
-        .update({
-      'totalTime': totalTime + DateTime.now().difference(ss.toDate()).inMilliseconds,
-      "isRunning":false,
-      
+
+    await FirebaseFirestore.instance.collection('tasks').doc(ids).update({
+      'totalTime':
+          totalTime + DateTime.now().difference(ss.toDate()).inMilliseconds,
+      "isRunning": false,
     });
-    int remainingTime = int.parse(deadlinez) - (totalTime / (1000 * 60 * 60)).round();
-    print(remainingTime >= 0 ? 'Task completed on time!' : 'Task exceeded the deadline!');
+    int remainingTime =
+        int.parse(deadlinez) - (totalTime / (1000 * 60 * 60)).round();
+    print(remainingTime >= 0
+        ? 'Task completed on time!'
+        : 'Task exceeded the deadline!');
 
     // Update Firebase with task completion details
     // await _firestore.collection('tasks').add({
@@ -303,14 +282,10 @@ final documents = snapshot.data!.docs;
     //   'completedOnTime': remainingTime >= 0,
     //   'timestamp': FieldValue.serverTimestamp(),
     // });
-      await FirebaseFirestore.instance
-        .collection('tasks')
-        .doc(ids)
-        .update({
-      "totalTime":totalTime,
-      "completeTime": remainingTime >= 0 ? "OnTime":"noTime",
-      "status":true,
-  
+    await FirebaseFirestore.instance.collection('tasks').doc(ids).update({
+      "totalTime": totalTime,
+      "completeTime": remainingTime >= 0 ? "OnTime" : "noTime",
+      "status": true,
     });
 
     // Reset the current task for the next one
@@ -318,6 +293,4 @@ final documents = snapshot.data!.docs;
     //   currentTask = Task(taskName: 'Example Task', deadlineHours: 8, startTime: DateTime.now());
     // });
   }
-  
-
 }
